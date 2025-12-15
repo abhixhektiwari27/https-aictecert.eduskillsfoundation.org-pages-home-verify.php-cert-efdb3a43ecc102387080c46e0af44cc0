543da0c54c7ef42e403e7355c4544f1f0e8b5520
abhixhektiwari27 <abh.tiwari2004@gmail.com>
2025-12-15 21:30:54 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,31 @@
+var app = angular.module('myApp', []);
+app.controller('certController', function ($scope, $timeout) {
+    // Extract cert parameter from URL
+    var urlParams = new URLSearchParams(window.location.search);
+    var certParam = urlParams.get('cert');
+    // Default valid cert ID from user request
+    var validCertId = "efdb3a43ecc102387080c46e0af44cc0";
+    // Mock Data
+    var mockData = {
+        certid: "efdb3a43ecc102387080c46e0af44cc0",
+        certissuedate: "25 Sep 2025",
+        studentname: "Abhishek  Tiwari",
+        institute: "G L Bajaj Institute of Technology & Management",
+        eventname: "ALTERYX DATA ANALYTICS PROCESS AUTOMATION VIRTUAL INTERNSHIP (JUL - SEP 2025)",
+        grade: ""
+    };
+    $scope.errmsg = "";
+    $scope.certnotverified = false;
+    $scope.certverified = false;
+    // Simulate loading
+    $timeout(function () {
+        if (certParam === validCertId || !certParam) {
+            // If ID matches or no ID provided (for demo purposes show success)
+            $scope.cert = mockData;
+            $scope.certverified = true;
+        } else {
+            $scope.certnotverified = true;
+            $scope.errmsg = "Sorry certificate details not found";
+        }
+    }, 500);
+});
